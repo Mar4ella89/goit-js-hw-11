ddf17182814375f406336134af08704acad8a2b4
Marat Baikin <3371335@gmail.com>
2022-12-12 23:54:56 +0200

    update

--- src/js/index.js
+++ src/js/index.js
@@ -3,34 +3,74 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 import Notiflix from 'notiflix';
 import GalleryApiService from './galleryService';
 import { markupPhotoGallery } from './template';
+import LoadMoreBtn from './load-more-btn';
 
-const lightbox = new SimpleLightbox('.gallery a', {
-  captionsData: 'alt',
-  captionPosition: 'bottom',
-  captionDelay: 250,
-  disableScroll: false,
-  scrollZoom: false,
-});
+// const lightbox = new SimpleLightbox('.gallery a', {
+//   captionsData: 'alt',
+//   captionPosition: 'bottom',
+//   captionDelay: 250,
+//   disableScroll: false,
+//   scrollZoom: false,
+// });
 
 const ref = {
   searchForm: document.querySelector('#search-form'),
   photosContainer: document.querySelector('.gallery'),
-  loadMoreBtn: document.querySelector('.load-more'),
+  // loadMoreBtn: document.querySelector('[data-action="load-more"]'),
 };
 
 const galleryApiService = new GalleryApiService();
+const loadMoreBtn = new LoadMoreBtn({
+  selector: '[data-action="load-more"]',
+  hidden: true,
+});
+console.log(loadMoreBtn);
 
-function render(hits) {
-  if (hits.length === 0) {
-    Notiflix.Notify.failure(
-      `Sorry, there are no images matching your search query. Please try again.`,
-      {
-        clickToClose: true,
-        timeout: 3000,
-      }
-    );
+function fetchSuccess({ totalHits }) {
+  if (totalHits != 0) {
+    Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`, {
+      clickToClose: true,
+      timeout: 3000,
+    });
+  }
+  
+}
+
+function fetchError() {
+  Notiflix.Notify.failure(`Page is not defined. Please try again.`, {
+    clickToClose: true,
+    timeout: 3000,
+  });
+}
+
+function fetchFailure() {
+  Notiflix.Notify.failure(
+    `Sorry, there are no images matching your search query. Please try again.`,
+    {
+      clickToClose: true,
+      timeout: 3000,
+    }
+  );
+}
+
+function render({ hits, totalHits }) {
+  if (totalHits === 0) {
+    fetchFailure();
+    loadMoreBtn.hide();
     return;
   }
+  // else if (hits.length === totalHits) {
+  //   loadMoreBtn.disable();
+  //   Notiflix.Notify.info(
+  //     `We're sorry, but you've reached the end of search results.`,
+  //     {
+  //       clickToClose: true,
+  //       timeout: 3000,
+  //     }
+  //   );
+  //   loadMoreBtn.hide();
+  // return;
+  // }
   const renderImgContainer = hits.map(markupPhotoGallery);
   ref.photosContainer.insertAdjacentHTML(
     'beforeend',
@@ -38,32 +78,35 @@ function render(hits) {
   );
 }
 
-function onFetchError() {
-  Notiflix.Notify.failure(`Page is not defined. Please try again.`, {
-    clickToClose: true,
-    timeout: 3000,
-  });
-}
-
 ref.searchForm.addEventListener('submit', onSearch);
-ref.loadMoreBtn.addEventListener('click', onLoadMore);
+// ref.loadMoreBtn.addEventListener('click', onLoadMore);
+loadMoreBtn.refs.button.addEventListener('click', onLoadMore);
 
 function onSearch(e) {
   e.preventDefault();
   galleryApiService.query = e.currentTarget.elements.searchQuery.value;
+
+  loadMoreBtn.show();
+  loadMoreBtn.disable();
   galleryApiService.resetPage();
 
   galleryApiService
     .galleryImg()
     .then(data => {
       clearGallery();
-      render(data.hits);
+      render(data);
+      fetchSuccess(data);
+      loadMoreBtn.enable();
     })
-    .catch(onFetchError);
+    // .catch(fetchError());
 }
 
 function onLoadMore() {
-  galleryApiService.galleryImg().then(data => render(data.hits));
+  loadMoreBtn.disable();
+  galleryApiService.galleryImg().then(data => {
+    render(data);
+    loadMoreBtn.enable();
+  });
 }
 
 function clearGallery() {
